efdc70b8cb182942d27e5124ef88c9f189fb67fb
Klaus Guan <guansdu@163.com>
2016-07-26 14:41:57 +0800

    update stl_heap.cpp

--- heap/stl_heap.cpp
+++ heap/stl_heap.cpp
@@ -1,12 +1,17 @@
+#include "my_priority_queue.h"
+
 #include <iostream>
 #include <vector>
-
 #include "my_heap.h"
+#include "my_priority_queue.h"
 
 using namespace std;
 
 int main()
 {
+	//----
+	//Heap
+	//----
 	int ia[9] = {0,1,2,3,4,8,9,3,5};
 	vector<int> ve(ia, ia+9);
 	MakeHeap<int> (ve.begin(), ve.end());
@@ -32,7 +37,22 @@ int main()
 	for (int i = 0; i < ve.size(); i++)
 		cout << ve[i] << " ";    //0 1 2 3 3 4 5 7 8 
 	cout << endl;
+	
+	//----
+	//Priority Queue
+	//----	
+	int ar[9] = {0, 1, 3, 5, 2, 4 ,8 ,6 ,7};
+	MyPriorityQueue<int> mpq(ar, 9);
+	cout << mpq.size() << endl;    //9
+	
+	cout << mpq.top() << endl;    //8
 
+	while (!mpq.empty())
+	{
+		cout << mpq.top() << " ";    //8 7 6 5 4 3 2 1 0
+		mpq.pop();
+	}
+	cout << endl;
 	getchar();
 	return 0;
 }
